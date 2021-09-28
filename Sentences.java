86525d148b4d8525af4dd660bdf0abb788e7402e
MrLives-For-Today <imcalzada03@gmail.com>
2021-09-27 19:42:19 -0500

    First Java Program - Operatinal!

---
+++ Sentences.java
@@ -0,0 +1,7 @@
+public class Sentences {
+    public static void main(String[] args) {
+        System.out.println("My name is Coding Dojo");
+        System.out.println("I am 100 years old. Wait...really?");
+        System.out.println("My hometown is Burbank, CA");
+    }
+}
